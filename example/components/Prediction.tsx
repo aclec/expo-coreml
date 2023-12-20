138ed64772f140c110de827ddd645493b3355727
aclec <anicetprodhomme@gmail.com>
2023-12-20 12:07:40 +0100

    Prediction

---
+++ example/components/Prediction.tsx
@@ -0,0 +1,89 @@
+import {Button, Text, View} from 'react-native';
+import * as ExpoCoreml from 'expo-coreml';
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from 'expo-image-picker';
+
+export default function Prediction() {
+
+    const clear = async () => {
+        let info = await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'modelPrediction.mlmodel');
+        if(info.exists) await FileSystem.deleteAsync(FileSystem.documentDirectory + 'modelPrediction.mlmodel');
+        info = await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'modelPrediction.mlmodelc');
+        if(info.exists) await FileSystem.deleteAsync(FileSystem.documentDirectory + 'modelPrediction.mlmodelc');
+        console.log( await FileSystem.readDirectoryAsync(FileSystem.documentDirectory as string))
+    }
+
+    const downLoadModel = async () => {
+        const info = await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'modelPrediction.mlmodel');
+        if(info.exists) await FileSystem.deleteAsync(FileSystem.documentDirectory + 'modelPrediction.mlmodel');
+        console.log("Start Download");
+        try {
+            await FileSystem.createDownloadResumable(
+                'http://localhost:3000/modelPredictionBrut.mlmodel',
+                FileSystem.documentDirectory + 'modelPrediction.mlmodel',
+                {}
+            ).downloadAsync();
+            console.log( await FileSystem.readDirectoryAsync(FileSystem.documentDirectory as string))
+        }catch (e) {
+            console.log(e)
+        }
+        console.log("End Download");
+    }
+
+    const compileModel = async () => {
+        console.log("Start Compile");
+        const url = await ExpoCoreml.compileModel(FileSystem.documentDirectory + 'modelPrediction.mlmodel');
+        if(!!url){
+            const info = await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'modelPrediction.mlmodelc');
+            if(info.exists) await FileSystem.deleteAsync(FileSystem.documentDirectory + 'modelPrediction.mlmodelc');
+            await FileSystem.copyAsync({from: url, to: FileSystem.documentDirectory + 'modelPrediction.mlmodelc'});
+            await FileSystem.deleteAsync(url);
+            console.log( await FileSystem.readDirectoryAsync(FileSystem.documentDirectory as string))
+        }
+        console.log("End Compile");
+    }
+
+
+    const handlePredict = async () => {
+
+        const images = await ImagePicker.launchImageLibraryAsync({
+            quality: 1
+        });
+        if(!!images.assets){
+            for (const image of images.assets){
+                // console.log(image.uri)
+                const res = await ExpoCoreml.predict(
+                    FileSystem.documentDirectory + 'modelPrediction.mlmodelc',
+                    image.uri,
+                );
+
+                const refactorSize = res.map(item => {
+                    return {
+                        ...item,
+                        boundingBox: {
+                            x: item?.boundingBox?.x * image.width,
+                            y: item?.boundingBox?.y * image.height,
+                            width: item?.boundingBox?.width * image.width,
+                            height: item?.boundingBox?.height * image.height
+                        }
+                    }
+                });
+
+                console.log(refactorSize);
+                console.log("Totals => ", refactorSize.length);
+
+            }
+        }
+    }
+
+
+    return (
+        <View style={{display: "flex", alignItems: "center", borderBottomWidth: 1, marginBottom: 10}}>
+            <Text style={{fontSize: 20}}>Prediction</Text>
+            <Button title={"Clear"} onPress={clear} color={"red"}/>
+            <Button title={"Download Model"} onPress={downLoadModel}/>
+            <Button title={"Compile"} onPress={compileModel}/>
+            <Button title={"Predict"} onPress={handlePredict}/>
+        </View>
+    );
+}
